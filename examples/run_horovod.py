cefa2bdb15a54811089ea48423ad96c162ad841a
Ting-Che Lin <ting-che.lin@petuum.com>
2020-03-09 13:49:21 -0400

    add run_horovod script

---
+++ examples/run_horovod.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-m','--machines', nargs='+', help='machine ips', required=True)
+
+args = parser.parse_args()
+
+for cnn_model in ['resnet101', 'densenet121', 'inceptionv3', 'vgg16']:
+    cmd = f"horovodrun -np {4 * len(args.machines)} -H {' '.join(args.machines)} -p 12345 python main_horovod.py --data_dir /data --train_epochs 10 --cnn_model={cnn_model}"
+    print(cmd)
+    os.system(cmd)
