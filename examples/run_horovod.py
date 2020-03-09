732f36eb5e4bc70f68e30fac58cefa759f3a5f1b
Ting-Che Lin <ting-che.lin@petuum.com>
2020-03-09 14:23:43 -0400

    replace space with comma

--- examples/run_horovod.py
+++ examples/run_horovod.py
@@ -9,6 +9,6 @@ parser.add_argument('-m','--machines', nargs='+', help='machine ips', required=T
 args = parser.parse_args()
 
 for cnn_model in ['resnet101', 'densenet121', 'inceptionv3', 'vgg16']:
-    cmd = f"horovodrun -np {4 * len(args.machines)} -H {' '.join(args.machines)} -p 12345 python main_horovod.py --data_dir /data --train_epochs 10 --cnn_model={cnn_model}"
+    cmd = f"horovodrun -np {4 * len(args.machines)} -H {','.join(args.machines)} -p 12345 python main_horovod.py --data_dir /data --train_epochs 10 --cnn_model={cnn_model}"
     print(cmd)
     os.system(cmd)
